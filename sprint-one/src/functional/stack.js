6fed6878033a72f66e83c573d41ec751510676d5
kfoxb <kfoxb@kfoxb.com>
2017-02-08 15:00:21 -0800

    Complete Stack function in functional instantiation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    var result = storage[size - 1];
+    //delete storage[lastIndex];
+    storage[size - 1] = undefined;
+    size--;
+    return result;
   };
 
   someInstance.size = function() {
-    return 0;
+    if (size <= 0) {
+      size = 0;
+    }
+    return size;
   };
 
   return someInstance;
