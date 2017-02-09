6cb0383f1b12a3c646815396b80572ec10753266
kfoxb <kfoxb@kfoxb.com>
2017-02-08 21:10:51 -0800

    Change functions to be declared inside methods object

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -8,20 +8,39 @@ var Stack = function() {
   return stack;
 };
 
-var stackMethods = {};
+// BOTH styles work: REMEMBER:
+// we can assign properties to objects in 2 ways
+// 1) use : notation inside object literal
+// 2) assign property outside of pre-declared object
 
-stackMethods.size = function() {
-  return this.length > 0 ? this.length : 0;
-}
+var stackMethods = {
+  size: function() {
+    return this.length > 0 ? this.length : 0;
+  },
+  push: function(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  pop: function() {
+    var result = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length--;
+    return result;
+  }
+};
 
-stackMethods.push = function(value) {
-  this.storage[this.length] = value;
-  this.length++;
-}
-
-stackMethods.pop = function() {
-  var result = this.storage[this.length - 1];
-  delete this.storage[this.length - 1];
-  this.length--;
-  return result;
-}
+// stackMethods.size = function() {
+//   return this.length > 0 ? this.length : 0;
+// }
+//
+// stackMethods.push = function(value) {
+//   this.storage[this.length] = value;
+//   this.length++;
+// }
+//
+// stackMethods.pop = function() {
+//   var result = this.storage[this.length - 1];
+//   delete this.storage[this.length - 1];
+//   this.length--;
+//   return result;
+// }
