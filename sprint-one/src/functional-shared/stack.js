3b5c6947a03e6b01aad9428b2f9e127bfd7a7b81
kfoxb <kfoxb@kfoxb.com>
2017-02-08 16:22:08 -0800

    complete stack data structure for functionally-shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = {};
+  stack.length = 0;
+  stack.storage = {};
+  _.extend(stack, stackMethods);
+  return stack;
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return this.length > 0 ? this.length : 0;
+}
 
+stackMethods.push = function(value) {
+  this.storage[this.length] = value;
+  this.length++;
+}
+
+stackMethods.pop = function() {
+  var result = this.storage[this.length - 1];
+  delete this.storage[this.length - 1];
+  this.length--;
+  return result;
+}
