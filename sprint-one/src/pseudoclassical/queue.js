17cf142759b912df45738cd4c9d133ad63041741
kfoxb <kfoxb@kfoxb.com>
2017-02-09 10:39:22 -0800

    Complete queue in pseudoclassical instantiation.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,4 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.storage = {};
 };
+
+Queue.prototype.size = function () {
+  return this.length > 0 ? this.length : 0;
+};
+
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.length] = value;
+  this.length++;
+}
+
+Queue.prototype.dequeue = function () {
+  var result = this.storage[0];
+  for (var i = 0; i < this.length; i++) {
+    this.storage[i] = this.storage[i + 1];
+  }
+  this.length--;
+  return result;
+}
