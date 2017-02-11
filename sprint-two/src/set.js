762dd7078d044ed5887b8abeec9a1d82fd7818a1
kfoxb <kfoxb@kfoxb.com>
2017-02-10 16:39:48 -0800

    Complete set data structure with prototypical instantiation

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return this[item] ? true : false;
 };
 
 setPrototype.remove = function(item) {
+  delete this[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+//all is O(1)
