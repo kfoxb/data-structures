e9f8a7dffa9b36cd168f23c737133761e3469983
kfoxb <kfoxb@kfoxb.com>
2017-02-11 10:00:47 -0800

    complete hash tables using prototypical instantiation

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,11 +9,12 @@ HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   //declare tuple
   var tuple = [k, v];
-  var bucket;
+  var bucket = this._storage.get(index);
+
   //if retrieve(k) returns array save array into var bucket
-  if (this._storage.get(index) instanceof Array) {
+  if (bucket instanceof Array && bucket.length > 0) {
     //push tuple into var bucket
-    bucket = this._storage.get(index);
+    // bucket = this._storage.get(index);
     for (var i = 0; i < bucket.length; i++) {
       if (bucket[i][0] === k) {
         bucket[i][1] = v;
@@ -35,12 +36,35 @@ HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   //bucket = get(index)
   var bucket = this._storage.get(index);
-
-  return bucket[0][1];
+  //iterate thru bucket
+  for (var i = 0; i < bucket.length; i++) {
+    //check for value = key
+    if (bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  //get bucket
+  var bucket = this._storage.get(index);
+  //iterate thru bucket
+  for (var i = 0; i < bucket.length; i++) {
+    //check key for value
+    if (bucket[i][0] === k) {
+      //re-assign value at key with value in key + 1
+      for (i; i < bucket.length; i++) {
+        bucket[i] = bucket[i + 1];
+      }
+      //remove last index of array
+      bucket.pop();
+      //break the inside for loop (stop searching for k)
+      break;
+    }
+  }
+  // set bucket
+  this._storage.set(index, bucket);
 };
 
 /*
